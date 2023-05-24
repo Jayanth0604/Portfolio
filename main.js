e2ad8394b5dbbeb33089e623e490f314beb7970b
Jayanth0604 <kumarjayanth473@gmail.com>
2023-05-24 12:28:57 +0530

    commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function changeBg(){
     var navbar = document.getElementById("navbar")
     var scrollColor = window.scrollY;
-   if(scrollColor <500 ){
+   if(scrollColor <200 ){
     navbar.classList.remove('bgColor');
    }else{
     navbar.classList.add('bgColor')
